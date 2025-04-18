bd6f91ffa36c849ebc89fb558b6802ce284cdf3d
Aron Barbosa de Oliveira <aron.b.96@gmail.com>
2025-04-18 00:14:35 -0300

    Update .eslintrc_next-no-error.cjs

--- configs/.eslintrc_next-no-error.cjs
+++ configs/.eslintrc_next-no-error.cjs
@@ -6,6 +6,7 @@ import reactPlugin from "eslint-plugin-react";
 import reactHooksPlugin from "eslint-plugin-react-hooks";
 import nextPlugin from "@next/eslint-plugin-next";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
 export default [
   ...new FlatCompat({
     baseDirectory: dirname(fileURLToPath(import.meta.url)),
@@ -20,25 +21,61 @@ export default [
     languageOptions: {
       parser: "@typescript-eslint/parser",
       parserOptions: {
-        ecmaVersion: 2020,
+        ecmaVersion: 2022,
         sourceType: "module",
         project: "./tsconfig.json",
+        ecmaFeatures: {
+          jsx: true,
+          globalReturn: false,
+          impliedStrict: true,
+        },
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2015,
+        ...globals.es2022,
       },
     },
-    rules: Object.fromEntries(
-      Object.entries({
-        ...eslintRecommended.rules,
-        ...reactPlugin.configs.recommended.rules,
-        ...reactHooksPlugin.configs.recommended.rules,
-        ...nextPlugin.configs.recommended.rules,
-        ...tsPlugin.configs.recommended.rules,
-      }).map(([name, setting]) => {
-        if (typeof setting === "string")
-          return [name, setting === "error" ? "warn" : setting];
-        if (!Array.isArray(setting)) return [name, setting];
-        const [level, ...opts] = setting;
-        return [name, [level === "error" ? "warn" : level, ...opts]];
-      })
-    ),
+    rules: {
+      ...Object.fromEntries(
+        Object.entries({
+          ...eslintRecommended.rules,
+          ...reactPlugin.configs.recommended.rules,
+          ...reactHooksPlugin.configs.recommended.rules,
+          ...nextPlugin.configs.recommended.rules,
+          ...tsPlugin.configs.recommended.rules,
+        }).map(([name, setting]) => {
+          if (typeof setting === "string")
+            return [name, setting === "error" ? "warn" : setting];
+          if (!Array.isArray(setting)) return [name, setting];
+          const [level, ...opts] = setting;
+          return [name, [level === "error" ? "warn" : level, ...opts]];
+        })
+      ),
+      "no-ternary": "off",
+      "no-case-declarations": "off",
+      "no-inline-comments": "off",
+      "no-console": "off",
+      "no-prototype-builtins": "off",
+      "require-jsdoc": "off",
+      "valid-jsdoc": "off",
+      "prefer-template": "off",
+      "spaced-comment": "off",
+      "no-unused-vars": "off",
+      "comma-dangle": "off",
+      "space-in-parens": "off",
+      "rest-spread-spacing": "off",
+      "object-curly-spacing": "off",
+      "array-bracket-newline": "off",
+      "space-before-function-paren": "off",
+      "no-restricted-globals": "off",
+      "react/display-name": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@next/next/no-html-link-for-pages": "off",
+      "@next/next/no-img-element": "off",
+    },
   },
 ];
